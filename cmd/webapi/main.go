bce8721375d7af86ff2ff1ca1931f3e7667bf0da
donnim1 <donnimesh60@gmail.com>
2024-12-21 05:46:45 +0100

    changed the backend structure completely

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/ardanlabs/conf"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"ssh
 	"github.com/sirupsen/logrus"
 	"math/rand"
 	"net/http"
